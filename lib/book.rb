4c70662d3b006527e5788f275761607cda0c1435
Alexa <amzeazas@comcast.net>
2015-08-19 11:09:18 -0700

    Add .==, .all and save methods to Book class.

--- lib/book.rb
+++ lib/book.rb
@@ -5,4 +5,25 @@ class Book
     @title = attributes.fetch(:title)
     @author = attributes.fetch(:author)
   end
+
+  define_method(:==) do |another_book|
+    self.title().==(another_book.title())
+  end
+
+  define_singleton_method(:all) do
+    returned_books = DB.exec("SELECT * FROM books;")
+    books = []
+    returned_books.each() do |book|
+      title = book.fetch("title")
+      author = book.fetch("author")
+      books.push(Book.new({:title => "The Hobbit", :author => "J.R.R. Tolkien"}))
+    end
+    books
+  end
+
+  define_method(:save) do
+    DB.exec("INSERT INTO books (title, author) VALUES ('#{@title}', '#{@author}');")
+  end
+
+
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,6 +1,13 @@
 require('spec_helper')
 
 describe('Book') do
+  describe('#==') do
+    it('is the same book if it has the same title') do
+      book1 = Book.new({:title => "The Hobbit", :author => "J.R.R. Tolkien"})
+      book2 = Book.new({:title => "The Hobbit", :author => "J.R.R. Tolkien"})
+      expect(book1).to(eq(book2))
+    end
+  end
 
   describe('#title') do
     it('returns the title of the book') do
