221bc7172bf82421862b5eb0cf2317859e35ea18
Alexa <amzeazas@comcast.net>
2015-08-19 10:48:40 -0700

    Add specs for .all and save methods to Book class.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -16,4 +16,18 @@ describe('Book') do
     end
   end
 
+  describe('.all') do
+    it('is empty at first') do
+      expect(Book.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('adds a book to the array of saved books') do
+      test_book = Book.new({:title => "The Hobbit", :author => "J.R.R. Tolkien"})
+      test_book.save()
+      expect(Book.all()).to(eq([test_book]))
+    end
+  end
+
 end
