283c5fb9ccd1061ed24eec478f5fb29329e00940
Alexa <amzeazas@comcast.net>
2015-08-19 10:34:52 -0700

    Add spec for author method to Book class.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,4 +8,12 @@ describe('Book') do
       expect(test_book.title()).to(eq("The Hobbit"))
     end
   end
+
+  describe('#author') do
+    it('returns the title of the book') do
+      test_book = Book.new({:title => "The Hobbit", :author => "J.R.R. Tolkien"})
+      expect(test_book.author()).to(eq("J.R.R. Tolkien"))
+    end
+  end
+
 end
