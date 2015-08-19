5701c42a975c258b95e1e0616655394773c8571f
Alexa <amzeazas@comcast.net>
2015-08-19 10:34:09 -0700

    Add spec for name method to Book class.

---
+++ spec/book_spec.rb
@@ -0,0 +1,11 @@
+require('spec_helper')
+
+describe('Book') do
+
+  describe('#title') do
+    it('returns the title of the book') do
+      test_book = Book.new({:title => "The Hobbit", :author => "J.R.R. Tolkien"})
+      expect(test_book.title()).to(eq("The Hobbit"))
+    end
+  end
+end
